a1a6f064becb88bad1d9c9d5cb44816054b7cc52
Archie Marshall <archiemarshall@users.noreply.github.com>
2026-03-07 01:07:53 +0000

    feat: add agent state

---
+++ agent/state.py
@@ -0,0 +1,8 @@
+from typing import Annotated, TypedDict
+
+from langgraph.graph.message import add_messages
+
+
+class AgentState(TypedDict):
+    messages: Annotated[list, add_messages]
+    repo_path: str
